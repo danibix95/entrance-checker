ca068fecedcaf716a6783270e6f0d35b9a3a3fec
danibix95 <dani95bissoli@gmail.com>
2023-12-28 17:54:51 +0100

    refactor: clean up the project to start from scratch

--- database/sql_scripts/import_data.sql
+++
@@ -1,5 +0,0 @@
-\c fdp_tickets;
-
--- \copy rely on current directory
-\copy tickets.attendees FROM '../csv/attendees.csv' WITH (FORMAT CSV, NULL 'null');
-\copy tickets.fdp_staff FROM '../csv/fdp_staff.csv' WITH (FORMAT CSV);
