ed991b9e63a74fcb069502714eddd0bc8ed17cb7
danibix95 <dani95bissoli@gmail.com>
2024-09-04 23:48:43 +0200

    feat(entrance_be): basic server example

---
+++ entrance_be/src/main.rs
@@ -0,0 +1,25 @@
+use axum::{http::StatusCode, routing::get, Json, Router};
+
+mod data;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new().route("/", get(hello));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> (StatusCode, Json<data::HelloMsg>) {
+    (
+        StatusCode::OK,
+        Json(data::HelloMsg {
+            msg: "Hello there!".into(),
+        }),
+    )
+}
